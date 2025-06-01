d7acc90ec6ae8b99f04a6c2827c484093b1445d7
shira-gabay <shira0556797080@gmail.com>
2025-06-01 10:59:54 +0300

    for render9

--- client-angular/src/app/services/exam.service.ts
+++ client-angular/src/app/services/exam.service.ts
@@ -3,12 +3,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Exam } from '../models/exam.model';
-
+import{environment} from '../../environments/environment'
 @Injectable({
   providedIn: 'root'
 })
 export class ExamService {
-  private apiUrl = 'http://localhost:5279/api/Exam/stats'; // כתובת מתאימה ל-API שלך
+  private apiUrl = `${environment.apiUrl}/api/Exam/stats`; // כתובת מתאימה ל-API שלך
 
   constructor(private http: HttpClient) {}
 
---
+++ client-angular/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://exampro-app.onrender.com' // או מה שה־API שלך
+};
